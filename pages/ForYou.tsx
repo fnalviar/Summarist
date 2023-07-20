7b3204862cb8852fb59b2b712f75e2dfac1aff46
fnalviar <nikka_alviar@outlook.com>
2023-07-20 02:52:36 -0400

    fetched data from api

--- pages/ForYou.tsx
+++ pages/ForYou.tsx
@@ -1,75 +1,135 @@
-import { AiOutlineMenu, AiOutlineSearch } from "react-icons/ai";
-
-import SelectedBooks from "@/components/library/SelectedBooks";
+import { BsPlayFill } from "react-icons/bs";
+import BookCard from "@/components/UI/BookCard";
+import SearchBackground from "@/components/library/SearchBackground";
 import Sidebar from "@/components/library/Sidebar";
 import useAuth from "@/hooks/useAuth";
 import { Book } from "@/types";
-import requests from "@/utils/requests";
+import Image from "next/image";
 import axios from "axios";
-import { useEffect } from "react";
+import requests from "@/utils/requests";
+import { useEffect, useState } from "react";
 
-interface Props {
-  selectedBook: Book;
-  recommendedBooks: Book[];
-  suggestedBooks: Book[];
-}
+// interface Props {
+//   selectedBook: Book;
+//   recommendedBooks: Book[];
+//   suggestedBooks: Book[];
+// }
 
-const ForYou = ({ selectedBook, recommendedBooks, suggestedBooks }: Props) => {
-  const { user, loading } = useAuth();
+// const ForYou = ({ selectedBook, recommendedBooks, suggestedBooks }: Props) => {
+const ForYou = () => {
+  const { user } = useAuth();
+  const [loading, setLoading] = useState(false);
+  const [selectedBook, setSelectedBook] = useState<Book | null>(null);
+  const [recommendedBooks, setRecommendedBooks] = useState<Book[]>([]);
+  const [suggestedBooks, setSuggestedBooks] = useState<Book[]>([]);
 
   async function fetchBooks() {
-    const selectedBook = (await axios.get(requests.fetchSelectedBook)).data[0];
-    const recommendedBooks = (await axios.get(requests.fetchRecommendedBooks))
-      .data;
-    const suggestedBooks = (await axios.get(requests.fetchSuggestedBooks)).data;
+    setLoading(true);
+    try {
+      const selectedBookResponse = (await axios.get(requests.fetchSelectedBook))
+        .data[0];
+      const recommendedBooksResponse = (
+        await axios.get(requests.fetchRecommendedBooks)
+      ).data;
+      const suggestedBooksResponse = (
+        await axios.get(requests.fetchSuggestedBooks)
+      ).data;
 
-    console.log("selectedBook", selectedBook);
-    console.log("recommendedBooks", recommendedBooks);
-    console.log("suggestedBooks", suggestedBooks);
+      setSelectedBook(selectedBookResponse);
+      setRecommendedBooks(recommendedBooksResponse);
+      setSuggestedBooks(suggestedBooksResponse);
+
+      console.log(selectedBookResponse);
+      console.log(recommendedBooksResponse);
+      console.log(suggestedBooksResponse);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+
+    return {
+      props: {
+        selectedBook,
+        recommendedBooks,
+        suggestedBooks,
+      },
+    };
   }
 
   useEffect(() => {
     fetchBooks();
   }, []);
 
-  if (!user) return null;
-
-  if (loading === null) return null;
-
   return (
+    // !loading && (
     <div id="foryou">
       <div className="content--wrapper">
-        <div className="search__background">
-          <div className="search__wrapper">
-            <div className="search__content">
-              <div className="search">
-                <div className="search__input--wrapper">
-                  <input
-                    type="text"
-                    className="search__input"
-                    placeholder="Search for books"
-                  />
-                  <div className="search__icon">
-                    <AiOutlineSearch className="search__icon--svg" />
+        <SearchBackground />
+        <div className="sidebar__overlay sidebar__overlay--hidden"></div>
+
+        <Sidebar />
+        <div className="row">
+          <div className="container">
+            <div className="selected__book__container">
+              <div className="section__header__title">
+                Selected just for you
+              </div>
+              <audio src=""></audio>
+              <a href="" className="selected__book">
+                <div className="selected__book--subtitle">
+                  {selectedBook?.subTitle}
+                </div>
+                <div className="selected__book--separator"></div>
+                <div className="selected__book--content">
+                  <figure className="selected--book__image--wrapper">
+                    <img
+                      src={selectedBook?.imageLink}
+                      alt="Book"
+                      className="book__img"
+                    />
+                  </figure>
+                  <div className="selected__book--info">
+                    <div className="selected__book--title">
+                      {selectedBook?.title}
+                    </div>
+                    <div className="selected__book--author">
+                      {selectedBook?.author}
+                    </div>
+                    <div className="selected__book--audio-wrapper">
+                      <div className="selected__book--icon">
+                        <BsPlayFill className="play--icon" />
+                      </div>
+                      <div className="selected__book--duration">
+                        1 mins 30 secs
+                      </div>
+                    </div>
                   </div>
                 </div>
+              </a>
+              <div>
+                <div className="section__header__title">
+                  Recommended For You
+                </div>
+                <div className="section__header__subtitle">
+                  We think you’ll like these
+                </div>
+                <BookCard />
               </div>
-              <div className="sidebar__toggle--btn">
-                <AiOutlineMenu className="menu__icon--svg" />
+              <div>
+                <div className="section__header__title">Suggested Books</div>
+                <div className="section__header__subtitle">
+                  Browse those books
+                </div>
+                <BookCard />
               </div>
             </div>
           </div>
         </div>
-        <div className="sidebar__overlay sidebar__overlay--hidden"></div>
-
-        <Sidebar />
-        <SelectedBooks
-          selectedBook={selectedBook}
-          recommendedBooks={recommendedBooks}
-          suggestedBooks={suggestedBooks}
-        />
       </div>
     </div>
+    // )
   );
 };
+
 export default ForYou;

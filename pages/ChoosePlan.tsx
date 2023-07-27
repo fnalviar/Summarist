ddbb700ee1198227c7d1f470f8825881f3e059db
fnalviar <nikka_alviar@outlook.com>
2023-07-27 01:39:38 -0400

    added plan components

---
+++ pages/ChoosePlan.tsx
@@ -0,0 +1,12 @@
+import PlanContent from "@/components/plan/PlanContent";
+import PlanHeader from "@/components/plan/PlanHeader";
+
+function ChoosePlan() {
+  return (
+    <div className="plan">
+      <PlanHeader />
+      <PlanContent />
+    </div>
+  );
+}
+export default ChoosePlan;

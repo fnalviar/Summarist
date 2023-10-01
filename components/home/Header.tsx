af30f617b3033d908af1eb9286bbbf0d620dad03
fnalviar <nikka_alviar@outlook.com>
2023-10-01 19:57:58 -0400

    added link on logo

--- components/home/Header.tsx
+++ components/home/Header.tsx
@@ -2,6 +2,7 @@ import { modalOpen } from "@/redux/modalSlice";
 import Image from "next/image";
 import { useDispatch } from "react-redux";
 import Logo from "../../assets/logo.png";
+import Link from "next/link";
 
 function Header() {
   const dispatch = useDispatch();
@@ -10,23 +11,25 @@ function Header() {
     <nav className="nav">
       <div className="nav__wrapper">
         <figure className="nav__img--mask">
-          <Image
-            className="nav__img"
-            src={Logo}
-            alt="logo"
-          />
+          <Link href="/for-you">
+            <Image className="nav__img" src={Logo} alt="logo" />
+          </Link>
         </figure>
         <ul className="nav__list--wrapper">
-          <li className="nav__list nav__list--login"
+          <li
+            className="nav__list nav__list--login"
             onClick={() => {
-              dispatch(modalOpen())
-            }}>Login</li>
+              dispatch(modalOpen());
+            }}
+          >
+            Login
+          </li>
           <li className="nav__list nav__list--mobile">About</li>
           <li className="nav__list nav__list--mobile">Contact</li>
           <li className="nav__list nav__list--mobile">Help</li>
         </ul>
       </div>
     </nav>
-  )
+  );
 }
-export default Header
+export default Header;
